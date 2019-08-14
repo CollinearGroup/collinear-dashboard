c580dbf27b224fbace0c1a2480bfdd503dfc7dc7
Dan Villa <dvilla@collineargroup.com>
2019-08-13 22:26:09 -0700

    add npm component stub

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import './App.css';
+import React from "react"
+import NpmMetrics from "./NpmMetrics"
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
+      <div className="widget"> Collinear Group Dashboard </div>
+      <div className="widget">
+        <NpmMetrics />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/NpmMetrics.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NpmMetrics() {
+
+  return (
+    <div>
+      NPM Metrics
+    </div>
+  );
+}
+
+export default NpmMetrics;
