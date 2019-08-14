17e4991359aba826c869fdb4a85df8311ccacca6
Dan Villa <dvilla@collineargroup.com>
2019-08-13 23:01:40 -0700

    add npm metrics for collinear modules

--- src/NpmMetrics.js
+++ src/NpmMetrics.js
@@ -1,12 +1,55 @@
-import React from 'react';
+import React from "react"
+import axios from "axios"
 
-function NpmMetrics() {
+const SMART_DEEP_SORT_NAME = "smart-deep-sort"
+const LICENSE_VALIDATOR_NAME = "license-validator"
+const PCAP_NAME = "pcap-ng-parser"
+const NPM_URL = "https://api.npmjs.org/downloads/point/last-week"
+const ONE_HOUR = 60 * 60 * 1000
 
-  return (
-    <div>
-      NPM Metrics
-    </div>
-  );
+class NpmMetrics extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      smartDeepSort: null,
+      licenseValidator: null,
+      pcap: null,
+    }
+  }
+
+  async componentDidMount() {
+    await this.updateData()
+    this.fetchInterval = setInterval(() => {
+      this.updateData()
+    }, ONE_HOUR)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.fetchInterval)
+  }
+
+  async updateData() {
+    this.setState({
+      smartDeepSort: await getModuleDownloads(SMART_DEEP_SORT_NAME),
+      licenseValidator: await getModuleDownloads(LICENSE_VALIDATOR_NAME),
+      pcap: await getModuleDownloads(PCAP_NAME),
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <div>Smart Deep Sort: {this.state.smartDeepSort}</div>
+        <div>License Validator: {this.state.licenseValidator}</div>
+        <div>PCAP: {this.state.pcap}</div>
+      </div>
+    )
+  }
 }
 
-export default NpmMetrics;
+async function getModuleDownloads(name) {
+  let response = await axios.get(`${NPM_URL}/${name}`)
+  return response.data.downloads
+}
+
+export default NpmMetrics
