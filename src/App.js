0b623d3792712f352617f6bfe6f19e0afa395a43
Dan Villa <dvilla@collineargroup.com>
2019-08-27 08:04:06 -0700

    inital prototype

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from "react"
+import { connect } from "react-redux"
 import NpmMetrics from "./npm-metrics/NpmMetrics"
 import "./App.scss"
 
-function App() {
+const mapStateToProps = (state) => {
+  return { ...state }
+}
+
+function App(props) {
   return (
     <div className="App">
-      <div className="widget"> Collinear Group Dashboard </div>
-      <div className="widget"> <NpmMetrics /> </div>
-      <div className="widget">  You're app here! </div>
-      <div className="widget">  You're app here! </div>
-      <div className="widget">  You're app here! </div>
-      <div className="widget">  You're app here! </div>
-      <div className="widget">  You're app here! </div>
+      <div className="widget">
+        <p>Collinear Group Dashboard</p>
+        <p> {props.time} </p>
+      </div>
+      <div className="widget">
+        <NpmMetrics />
+      </div>
+      <div className="widget"> You're app here! </div>
+      <div className="widget"> You're app here! </div>
+      <div className="widget"> You're app here! </div>
+      <div className="widget"> You're app here! </div>
+      <div className="widget"> You're app here! </div>
     </div>
   )
 }
 
-export default App
+export default connect(mapStateToProps)(App)
--- src/index.js
+++ src/index.js
@@ -1,6 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.scss"
+import App from "./App"
+import { configureStore, createSlice } from "redux-starter-kit"
+import { Provider } from "react-redux"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create a default action with name, payload
+const { actions, reducer } = createSlice({
+  initialState: {
+    time: new Date().toTimeString(),
+    id: Date.now()
+  },
+  reducers: {
+    setTime: (state, { payload }) => {
+      // TODO: validate props?
+      return payload
+    }
+  }
+})
+const { setTime } = actions
+
+// Configure store
+const store = configureStore({
+  reducer: reducer
+})
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+)
+
+// Drive some default updates
+setInterval(() => {
+  const payload = {
+    time: new Date().toTimeString(),
+    id: Date.now()
+  }
+  store.dispatch(setTime(payload))
+}, 1000)
