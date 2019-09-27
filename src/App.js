0b34e12a25bc460f3d2066a9c08a636e0db882e4
Dan Villa <dvilla@collineargroup.com>
2019-09-27 12:46:13 -0700

    add shuffle of layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import React from "react"
 import "./App.scss"
+import _ from "lodash"
 
 // Import your plugin
 import NpmMetrics from "./npm-metrics"
 import Info from "./info"
 
 // Add your plugin
-export default function App(props) {
-  return (
-    <div className="App">
-      <div className="widget">
-        <Info />
-      </div>
-      <div className="widget">
-        <NpmMetrics />
-      </div>
-      <div className="widget"> You're app here! </div>
-      <div className="widget"> You're app here! </div>
-      <div className="widget"> You're app here! </div>
-      <div className="widget"> You're app here! </div>
-      <div className="widget"> You're app here! </div>
-    </div>
-  )
+export default function App() {
+  let componentList = [
+    <Info />,
+    <NpmMetrics />,
+    getPlaceholder(),
+    getPlaceholder(),
+    getPlaceholder(),
+    getPlaceholder(),
+    getPlaceholder()
+  ]
+
+  function getPlaceholder() {
+    return <span> Hi Brian, Your're app here!</span>
+  }
+
+  return <GridContainer componentList={componentList} />
 }
+
+export class GridContainer extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      list: this.getShuffledList()
+    }
+  }
+  componentDidMount() {
+    // TODO: rename
+    this.fetchInterval = setInterval(() => {
+      this.setState({
+        list: this.getShuffledList()
+      })
+    }, 5000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.fetchInterval)
+  }
+
+  getShuffledList() {
+    return _.shuffle(this.props.componentList).map(el => {
+      return <div className="box">{el}</div>
+    })
+  }
+
+  render() {
+    return <div className="wrapper"> {this.state.list} </div>
+  }
+}
+
+// export function Grid() {
+//   function getShuffledList() {
+//     return _.shuffle(this.props.componentList).map(el => {
+//       return <div className="box">{el}</div>
+//     })
+//   }
+
+//   return <div className="wrapper"> {this.getShuffledList()} </div>
+// }
