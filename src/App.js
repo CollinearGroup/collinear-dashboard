abf5de4b1e24478d4054b3fc67045243fe8cf0f4
Dan Villa <dvilla@collineargroup.com>
2019-09-28 21:35:51 -0700

    Got layout going

--- src/App.js
+++ src/App.js
@@ -8,73 +8,89 @@ import Info from "./info"
 
 // Add your plugin
 export default function App() {
-  let info = (
-    <div className="theme1-box">
-      <Info />
-    </div>
-  )
-  let npmMetrics = (
-    <div className="theme1-box">
-      <NpmMetrics />,
-    </div>
-  )
-
-  let componentList = [
-    info,
-    npmMetrics,
-    getPlaceholder(),
-    getPlaceholder(),
-    getPlaceholder()
-  ]
-
-  function getPlaceholder() {
-    return <div className="theme2-box"> Hi Brian, Your're app here!</div>
-  }
-
-  return <GridContainer componentList={componentList} />
+  return <GridContainer />
 }
 
 export class GridContainer extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      list: this.props.componentList
+      sideBarPosition: "right"
     }
   }
+
   componentDidMount() {
+    let INTERVAL_TIME_MS = 10 * 1000
     this.updateDisplayInterval = setInterval(() => {
       this.setState({
-        // list: this.getShuffledList()
-        list: this.getRotatedList()
+        sideBarPosition: this.togglePosition()
       })
-    }, 5000)
+    }, INTERVAL_TIME_MS)
   }
 
   componentWillUnmount() {
     clearInterval(this.updateDisplayInterval)
   }
 
-  getShuffledList() {
-    return _.shuffle(this.props.componentList)
-  }
-
-  getRotatedList() {
-    console.log(this.state.list)
-    let list = [...this.state.list]
-    let el = list.shift()
-    list.push(el)
-    return list
-  }
-
-  renderWithFeature(list) {
-    // First element is "featured"
-    let result = []
-    result.push(<div className="featured-grid">{list[0]}</div>)
-    let wrapper = <div className="summary-grid"> {list.slice(1)} </div>
-    return result.concat(wrapper)
-  }
-
   render() {
-    return this.renderWithFeature(this.state.list)
+    let { sideBarPosition } = this.state
+    return (
+      <div className={`grid-layout-${sideBarPosition}`}>
+        <MainContent />
+        <SideBar />
+      </div>
+    )
   }
+
+  togglePosition() {
+    if (this.state.sideBarPosition === "right") {
+      return "left"
+    }
+    return "right"
+  }
+}
+
+export function MainContent() {
+  function getPlaceholder() {
+    return <div>Your app here!</div>
+  }
+  return (
+    <div className="grid-main">
+      <div className="stretch box">{getPlaceholder()}</div>
+      <div className="box">
+        <NpmMetrics />
+      </div>
+      <div className="box">{getPlaceholder()}</div>
+      <div className="stretch box">{getPlaceholder()}</div>
+    </div>
+  )
+}
+
+export function SideBar() {
+  return (
+    <div className="grid-sidebar">
+      <div className="box">
+        <Mission />
+      </div>
+      <div className="box">
+        <Info />
+      </div>
+    </div>
+  )
+}
+
+export function Mission() {
+  return (
+    <div>
+      <h2>Collinear Software Engineering</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        nemo odit ipsam impedit reprehenderit laudantium cum corporis,
+        laboriosam optio? Possimus cumque consequatur in minima? Dolore
+        reiciendis laborum amet, perspiciatis minus est fugit, similique rerum,
+        vero sit velit officia? Facilis, quidem dolore. Voluptate commodi
+        repudiandae deserunt reprehenderit unde, nam aut dolorem.
+      </p>
+    </div>
+  )
 }
