4e84477a46f50755ee8461ba3a91f8f8e16f2716
Dan Villa <dvilla@collineargroup.com>
2019-10-02 09:15:50 -0700

    add env var support

--- src/foosball/data/fireStore.js
+++ src/foosball/data/fireStore.js
@@ -1,8 +1,7 @@
 const firebase = require("firebase");
 require("firebase/firestore");
 
-const FIREBASE_CONFIG = null;
-//replace with FIREBASE_CONFIG found on confluence
+const FIREBASE_CONFIG = JSON.parse(process.env.REACT_APP_FIREBASE)
 
 firebase.initializeApp(FIREBASE_CONFIG);
 const db = firebase.firestore();
