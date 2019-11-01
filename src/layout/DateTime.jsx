05795a4c504f6ae216d22e368b1b5957d52d457f
Dan Villa <dvilla@collineargroup.com>
2019-11-01 16:51:40 -0700

    chore: clean up date time

--- src/layout/DateTime.jsx
+++ src/layout/DateTime.jsx
@@ -1,12 +1,35 @@
 import React from "react"
 import { getDate, getTime } from "./headerUtils"
+
+const ONE_MINUTE = 60 * 1000
+
 export default class DateTime extends React.Component {
+  constructor(props) {
+    super(props)
+    const now = new Date(Date.now())
+    this.state = {
+      date: getDate(now),
+      time: getTime(now)
+    }
+
+    // Note that since this component is mounted once and forever this is ok.
+    setInterval(() => {
+      const now = new Date(Date.now())
+      this.setState({
+        date: getDate(now),
+        time: getTime(now)
+      })
+    }, ONE_MINUTE)
+  }
+
   render() {
     return (
       <div id="dateTime" className="padding justify-right">
-        <div id="date">{getDate(new Date(Date.now()))}</div>
-        <div class="vertical-separator"></div>
-        <div id="time">{getTime(new Date(Date.now()))}</div>
+        <div id="dt-content">
+          <strong>{this.state.date}</strong>
+          <span class="vertical-separator"></span>
+          <span>{this.state.time}</span>
+        </div>
       </div>
     )
   }
