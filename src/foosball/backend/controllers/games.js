5be7da2c04da99b08c48d2f0085dd5b4fbf4fecb
Henry Turner <hturner@collineargroup.com>
2019-11-04 12:01:50 -0800

    fix ratings calculations and set rating_coefficient_ceiling to 250

--- src/foosball/backend/controllers/games.js
+++ src/foosball/backend/controllers/games.js
@@ -10,22 +10,36 @@ const uniq = require('lodash.uniq')
 const WEIGHT_FACTOR = 1000
 
 // K
-const RATING_COEFFICIENT_CEILING = 50
+const RATING_COEFFICIENT_CEILING = 250
 const RATING_COEFFICIENT_FLOOR = 50
 const QUALIFIER_MATCHES = 10
 
 const ratingCoefficient = (previousGamesPlayed) => {
   const floorCeilingDiff = RATING_COEFFICIENT_CEILING - RATING_COEFFICIENT_FLOOR
   if (previousGamesPlayed < QUALIFIER_MATCHES) {
-    return (floorCeilingDiff - ((floorCeilingDiff/QUALIFIER_MATCHES) * previousGamesPlayed)) + RATING_COEFFICIENT_FLOOR
+    const RATING_COEFFICIENT = (floorCeilingDiff - ((floorCeilingDiff/QUALIFIER_MATCHES) * previousGamesPlayed)) + RATING_COEFFICIENT_FLOOR
+    return RATING_COEFFICIENT
   }
   return RATING_COEFFICIENT_FLOOR
 }
 
 // S
-const calculateScorePercentage = (points) => {
-  let modifier = 0.2
-  return points * modifier
+// this equation assigns rating points purely based on whether a team won or lost a match
+// const calculateScorePercentage = (points) => {
+//   if (points === 5) {
+//     const modifier = 0.2
+//     return points * modifier
+//   }
+//   return 0
+// }
+
+// S
+// this equation assigns rating points based on how close the match was based on the score difference
+const calculateScorePercentage = (teamPoints, opponentPoints) => {
+  const modifier = 0.1
+  let pointsDifference = teamPoints - opponentPoints
+  const scorePercentage = (pointsDifference * modifier) + 0.5
+  return scorePercentage
 }
 
 const teamRatingAverage = (playerOneRating, player2Rating) => {
@@ -35,18 +49,18 @@ const teamRatingAverage = (playerOneRating, player2Rating) => {
 const calculateWinExpectancy = (playerTeamRating, opponentTeamRating, weightFactor) => {
   // We = 1/ (10 (-ratingDifference/weightFactor) + 1)
   const ratingDifference = opponentTeamRating - playerTeamRating
-  return 1/((10 * (-ratingDifference/weightFactor)) + 1)
+  const winExpectancy = (1/((10 ** (ratingDifference/weightFactor)) + 1))
+  return winExpectancy
 }
 
-const calculateNewRating = ({ playerRating, teammateRating, opponentDefRating, opponentOffRating, teamPoints, previousGamesPlayed }) => {
+const calculateNewRating = ({ playerRating, teammateRating, opponentDefRating, opponentOffRating, teamPoints, opponentPoints, previousGamesPlayed }) => {
   const originalTeamRating = teamRatingAverage(playerRating, teammateRating)
   const originalOpponentRating = teamRatingAverage(opponentDefRating, opponentOffRating)
   const winExpectancy = calculateWinExpectancy(originalTeamRating, originalOpponentRating, WEIGHT_FACTOR)
-  const scorePercentage = calculateScorePercentage(teamPoints)
+  const scorePercentage = calculateScorePercentage(teamPoints, opponentPoints,)
   const newPlayerRating = playerRating + (ratingCoefficient(previousGamesPlayed) * (scorePercentage - winExpectancy))
 
-  //TODO: Make rating a float...
-  return Math.round(newPlayerRating)
+  return newPlayerRating
 }
 
 class GamesController extends Controller {
@@ -67,10 +81,10 @@ class GamesController extends Controller {
     const { current_rating: black_off_rating, games_played: black_off_games_played } = await UsersModel.one(black_off)
 
     let playersToUpdate = uniqBy([
-      { playerId: red_def, previousGamesPlayed: red_def_games_played, playerRating: red_def_rating, teammateRating: red_off_rating, opponentDefRating: black_def_rating, opponentOffRating: black_off_rating, teamPoints: red_points},
-      { playerId: red_off, previousGamesPlayed: red_off_games_played, playerRating: red_off_rating, teammateRating: red_def_rating, opponentDefRating: black_def_rating, opponentOffRating: black_off_rating, teamPoints: red_points},
-      { playerId: black_def, previousGamesPlayed: black_def_games_played, playerRating: black_def_rating, teammateRating: black_off_rating, opponentDefRating: red_def_rating, opponentOffRating: red_off_rating, teamPoints: black_points},
-      { playerId: black_off, previousGamesPlayed: black_off_games_played, playerRating: black_off_rating, teammateRating: black_def_rating, opponentDefRating: red_def_rating, opponentOffRating: red_off_rating, teamPoints: black_points}
+      { playerId: red_def, previousGamesPlayed: red_def_games_played, playerRating: red_def_rating, teammateRating: red_off_rating, opponentDefRating: black_def_rating, opponentOffRating: black_off_rating, teamPoints: red_points, opponentPoints: black_points},
+      { playerId: red_off, previousGamesPlayed: red_off_games_played, playerRating: red_off_rating, teammateRating: red_def_rating, opponentDefRating: black_def_rating, opponentOffRating: black_off_rating, teamPoints: red_points, opponentPoints: black_points},
+      { playerId: black_def, previousGamesPlayed: black_def_games_played, playerRating: black_def_rating, teammateRating: black_off_rating, opponentDefRating: red_def_rating, opponentOffRating: red_off_rating, teamPoints: black_points, opponentPoints: red_points},
+      { playerId: black_off, previousGamesPlayed: black_off_games_played, playerRating: black_off_rating, teammateRating: black_def_rating, opponentDefRating: red_def_rating, opponentOffRating: red_off_rating, teamPoints: black_points, opponentPoints: red_points}
     ], 'playerId')
 
     const updatedPlayers = []
