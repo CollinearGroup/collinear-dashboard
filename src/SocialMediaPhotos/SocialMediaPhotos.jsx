7d1e7302cd8ba2e373e4e73714724065c858e143
Stephen Hunter <shunter@01010000-01110010-01101111.local>
2019-11-15 11:55:59 -0800

    User collinear cloudinary for photos

--- src/SocialMediaPhotos/SocialMediaPhotos.jsx
+++ src/SocialMediaPhotos/SocialMediaPhotos.jsx
@@ -1,18 +1,7 @@
 import React, { Component } from 'react'
-import { Image, Video, Transformation, CloudinaryContext } from 'cloudinary-react';
-
+import { Image, CloudinaryContext } from 'cloudinary-react';
 import axios from 'axios'
-
-// replace these with the photos we actually wanna use
-// import img2 from './images/dog2.jpeg'
-// import img3 from './images/dog3.jpeg'
-// import img4 from './images/dog4.jpeg'
-// import img5 from './images/dog5.jpeg'
-// import img6 from './images/dog6.jpeg'
-// import img7 from './images/dog7.jpeg'
-// import img8 from './images/dog8.jpeg'
 import './socialMediaPhotos.css'
-let img1 = 'https://www.dropbox.com/sh/z5pa1ntcberp7c3/AACA73SJ1t1ooh40080-TqK0a?dl=0&preview=20190803_141205_resized.jpg'
 
 export default class SocialMediaPhotos extends Component {
   state = {
@@ -21,21 +10,20 @@ export default class SocialMediaPhotos extends Component {
   }
 
   async componentDidMount() {
-    try{
+    try {
       let res = await axios.get("/images", {
         auth: {
-          username:228249614672283,
-          password:'Y4d_pgKoR1-XkktifCFxh7KbdLI'
+          username: process.env.CLOUDINARY_USER,
+          password: process.env.CLOUDINARY_PW
         },
-        mode:'no-cors'
+        mode: 'no-cors'
       })
       let ids = res.data.resources.map(img => img.public_id)
-      this.setState({images: ids}, console.log(this.state.images))
-    }catch(err) {
-      console.log('heeeey',err)
+      this.setState({ images: ids }, console.log(this.state.images))
+    } catch (err) {
+      console.log('heeeey', err)
     }
     this.startSlideShow()
-
   }
 
   componentWillUnmount() {
@@ -48,7 +36,6 @@ export default class SocialMediaPhotos extends Component {
       this.setState({ photoIndex: newIndex })
     }, 10000)
   }
-
 
   render() {
     const { photoIndex, images } = this.state
