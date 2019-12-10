10a39dedd4df1a5410f4611c82c7bd2a2cdde1a9
Jeffrey Tackett <jtackett@Jeffreys-MacBook-Pro.local>
2019-12-10 11:42:07 -0800

    Add Fullscreen When Logo Clicked

--- src/layout/Logo.jsx
+++ src/layout/Logo.jsx
@@ -1,9 +1,14 @@
 import React from "react"
 
 export default class Logo extends React.Component {
+
+  fullscreen() {
+    document.documentElement.requestFullscreen();
+  }
+
   render() {
     return (
-      <div id="logo-container" className="padding">
+      <div id="logo-container" className="padding" onClick={ () => this.fullscreen() } style={{ cursor: 'pointer' }}>
         <div id="logo-primary">
           collinear
           <div id="logo-secondary">GROUP</div>
