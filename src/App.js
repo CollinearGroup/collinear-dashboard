0847e5ca1e867c62450132fe0be2714a8fd465e2
Jeffrey Tackett <jtackett@Jeffreys-MacBook-Pro.local>
2019-12-12 10:51:59 -0800

    Hide Cursor When Idle and Show Shadow When Needed

--- src/App.js
+++ src/App.js
@@ -13,6 +13,29 @@ export default function App() {
 }
 
 export class GridContainer extends React.Component {
+  componentDidMount() {
+    (function() {
+      var mouseTimer = null, cursorVisible = true;
+  
+      function disappearCursor() {
+          mouseTimer = null;
+          document.body.style.cursor = "none";
+          cursorVisible = false;
+      }
+  
+      document.onmousemove = function() {
+          if (mouseTimer) {
+              window.clearTimeout(mouseTimer);
+          }
+          if (!cursorVisible) {
+              document.body.style.cursor = "default";
+              cursorVisible = true;
+          }
+          mouseTimer = window.setTimeout(disappearCursor, 5000);
+      };
+    })();
+  }
+
   wrapInId = (id, component) => {
     return (
       <div id={id} className="box padding">
