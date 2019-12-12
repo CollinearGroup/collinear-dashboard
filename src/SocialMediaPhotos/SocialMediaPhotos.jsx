f3086863d91ac1fad16fbd9fc2168848ee09a53c
Jeffrey Tackett <jtackett@Jeffreys-MacBook-Pro.local>
2019-12-12 15:44:55 -0800

    Don't Use Auto Format

--- src/SocialMediaPhotos/SocialMediaPhotos.jsx
+++ src/SocialMediaPhotos/SocialMediaPhotos.jsx
@@ -47,7 +47,7 @@ export default class SocialMediaPhotos extends Component {
       <div className="slideshow-container" style={style}>
         <div className="slideshow-photo" style={style}>
           <CloudinaryContext cloudName="collinear-group" >
-            <Image publicId={images[photoIndex]} fetchFormat="auto" width="auto" height="auto" radius="5" gravity="auto" background="auto" crop="fill_pad" />
+            <Image publicId={images[photoIndex]} width="auto" height="auto" radius="5" gravity="auto" background="auto" crop="fill_pad" />
           </CloudinaryContext>
         </div>
       </div>
