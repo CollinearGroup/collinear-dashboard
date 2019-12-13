0bcc431dbb89df5ff99c170c09345fd4d30dcfe9
Stephen Hunter <hunter98031@hotmail.com>
2019-12-13 11:23:44 -0500

    Initial setup of UI for message board

---
+++ src/message-board/MessageBoard.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react"
+import "./MessageBoard.css"
+
+class MessageBoard extends Component {
+  render() {
+    return (
+      <div>Message Board</div>
+    )
+  }
+}
+
+export default MessageBoard
