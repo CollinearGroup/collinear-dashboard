74d318744059e15eb7002179867a17bfec210c37
Dan Villa <dvilla@collineargroup.com>
2020-03-04 08:47:04 -0800

    feat: update app name

--- picture-uploader/res/upload-image-script.sh
+++ picture-uploader/res/upload-image-script.sh
@@ -1,5 +1,6 @@
-URL=http://localhost:9200/upload;
+# URL=http://localhost:9200/upload;
+URL=http://dashboard.collineargroup.com/picture-uploader/upload;
 for f in images/*; do
   echo "Uploading -> $f";
-  curl -H "pic-api-key:pic-api-key" -F file="@$f" $URL
+  curl -H "pic-api-key:b2ho-aW1hc@msK!" -F file="@$f" $URL
 done
