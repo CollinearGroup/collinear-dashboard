e934ae40b517f2b3620f37baf246df1619f758a7
Dan Villa <dvilla@collineargroup.com>
2020-03-04 17:36:37 -0800

    feat: move to fs based storage and update volume usage

--- picture-uploader/res/upload-image-script.sh
+++ picture-uploader/res/upload-image-script.sh
@@ -1,6 +1,8 @@
 # URL=http://localhost:9200/upload;
-URL=http://dashboard.collineargroup.com/picture-uploader/upload;
+URL=http://localhost/picture-uploader/upload;
+# URL=http://dashboard.collineargroup.com/picture-uploader/upload;
+PASS=pic-api-key
 for f in images/*; do
   echo "Uploading -> $f";
-  curl -H "pic-api-key:b2ho-aW1hc@msK!" -F file="@$f" $URL
+  curl -H "pic-api-key:$PASS" -F file="@$f" $URL
 done
--- picture-uploader/src/store.ts
+++ picture-uploader/src/store.ts
@@ -1,118 +1,47 @@
-import AWS from "aws-sdk"
-import fs, { ReadStream } from "fs"
+import fs from "fs"
 import { Readable } from "stream"
-AWS.config.update({ region: "west-us" })
-const endpoint = process.env.S3_ENDPOINT
 
-const mockCredentials = {
-  accessKeyId: "access-key-id",
-  secretAccessKey: "secret-access-key"
-}
-const s3client = new AWS.S3({
-  credentials: mockCredentials,
-  endpoint,
-  s3ForcePathStyle: true
-})
-const bucketName = process.env.AWS_BUCKET_NAME || "default"
-const bucketOptions = {
-  Bucket: bucketName
-}
+const STORE_PATH = process.env.STORE_PATH || "./tmp/uploads"
 
 export const storeFile = async (name: string, filePath: string) => {
-  console.log('storing file of', name, 'as', filePath)
+  console.log("storing file:", filePath, "with name:", name)
   const fileStream = fs.createReadStream(filePath)
-  await save(name, fileStream)
-}
-
-export const save = async (name: string, fileStream: ReadStream) => {
-  console.log('saving file of', name)
-  const uploadParams = {
-    Bucket: bucketName,
-    Body: fileStream,
-    Key: name
-  } as AWS.S3.PutObjectRequest
-
+  const savePath = `${STORE_PATH}/${name}`
+  const pipe = fileStream.pipe(fs.createWriteStream(savePath))
   return new Promise((resolve, reject) => {
-    fileStream.on("error", function(err) {
-      console.log("File Error", err)
+    pipe.on("error", () => {
       reject()
     })
-
-    // call S3 to retrieve upload file to specified bucket
-    s3client.upload(uploadParams, function(err, data) {
-      if (err) {
-        console.log("Storage Upload Error", err)
-        return reject()
-      }
-      console.log("Storage Upload Success", data.Location)
-      return resolve()
+    pipe.on("close", () => {
+      resolve()
     })
   })
 }
 
 export const getRandomFile = async (): Promise<Readable> => {
-  console.log('getting random file')
-  const objectsInBucket = await listObjectsInBucket()
-  const bucketItemsLength = objectsInBucket?.Contents?.length
-  if (bucketItemsLength === undefined || bucketItemsLength === null || bucketItemsLength === 0) {
-    throw new Error("There are no pictures to show.")
+  console.log("getting random file")
+  const files = await fs.promises.readdir(STORE_PATH)
+  if (files.length === 0) {
+    throw new Error("No files have been uploaded yet.")
   }
-  const randomPictureIndex = Date.now() % bucketItemsLength
-  const bucketObjectKey = objectsInBucket?.Contents?.[randomPictureIndex]?.Key
-  const pictureStream = getObject(bucketObjectKey)
-  return pictureStream
+  const randomFileIndex = Date.now() % files.length
+  const filePath = `${STORE_PATH}/${files[randomFileIndex]}`
+  return fs.createReadStream(filePath)
 }
 
-export const getObject = (Key: string = ""): Readable => {
-  const pts = {
-    Key,
-    Bucket: bucketName
+export const init = async () => {
+  console.log("initializing")
+  try {
+    const stats = await fs.promises.stat(STORE_PATH)
+    if (stats.isDirectory()) {
+      console.log("Storage directory is already created:", STORE_PATH)
+      return
+    }
+    console.log('You have really big problems');
+  } catch (error) {
+    console.log("Storage directory created:", STORE_PATH)
+    await fs.promises.mkdir(STORE_PATH)
+    return
   }
-  return s3client.getObject(pts).createReadStream()
 }
-
-export const listObjectsInBucket = (): Promise<AWS.S3.ListObjectsOutput> => {
-  return new Promise((resolve, reject) => {
-    s3client.listObjects(bucketOptions, function(err, data) {
-      if (err) {
-        reject(err)
-      }
-      resolve(data)
-    })
-  })
-}
-
-export const initializeStoreWithBucket = () => {
-  const setupBucketsInterval = setInterval(() => {
-    s3client.listBuckets((err, data) => {
-      if (err) {
-        console.log(
-          "Unable to validate storage service is ready, will retry.",
-          err.message
-        )
-        return
-      }
-      const existingBucket = data.Buckets?.find(el => el.Name === bucketName)
-      if (!existingBucket) {
-        const bucketOptions = {
-          Bucket: bucketName
-        }
-        s3client.createBucket(bucketOptions, err => {
-          if (err) {
-            console.log(
-              "Unable to validate storage service is ready, will retry.",
-              err.message
-            )
-            return
-          }
-          console.log("Created new bucket of", bucketName)
-          clearInterval(setupBucketsInterval)
-        })
-        return
-      }
-      console.log("Found existing bucket of", bucketName)
-      clearInterval(setupBucketsInterval)
-    })
-  }, 10000)
-}
-initializeStoreWithBucket()
+init()
