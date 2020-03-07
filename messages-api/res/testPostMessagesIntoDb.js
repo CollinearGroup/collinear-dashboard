c858ff067d0a690465e85409990a33b1a1ce1434
Dan Villa <dvilla@collineargroup.com>
2020-03-06 23:19:37 -0800

    feat: got hmac figured out in test scripts and verified it with the actual bot

--- messages-api/res/testPostMessagesIntoDb.js
+++ messages-api/res/testPostMessagesIntoDb.js
@@ -1,95 +1,41 @@
 const crypto = require("crypto")
-const baseURL = "http://localhost"
-const targetPort = 8011
-const serverId = "APP"
-const secretKey = "development"
-const issuedKeyAt = 1557794619
-const randomString = "WINTERBOOTS"
-
-const newMessage = {
-  text: "helloworld from team awesome (dan, and lee)",
-  from: "the dream team"
-}
-// const message = JSON.stringify(newMessage)
-
-// const hash = crypto.createHmac("sha256", secretKey).update(message)
-// console.log("first", hash)
-// // const digestHex = hash.digest("hex")
-// // console.log("hex", digestHex)
-// const digest64 = hash.digest("base64")
-// console.log("base64", digest64)
-// process.exit()
-
-// function createBase64Hash(data, hashAlg = "sha256") {
-//   return crypto
-//     .createHash(hashAlg)
-//     .update(data)
-//     .digest("base64")
-// }
-
-// function createHMACBase64Hash(data, hashAlg = "sha256") {
-//   return crypto
-//     // .createHmac(hashAlg, Buffer.from(secretKey, "base64"))
-//     .createHmac(hashAlg, secretKey)
-//     .update(data)
-//     .digest("base64")
-// }
-
-// function getEpochSinceIssued() {
-//   const currentEpochDate = Math.floor(new Date().getTime() / 1000.0)
-//   return currentEpochDate - issuedKeyAt
-// }
-
-// function createHMACAuth(domain, route, method, payload) {
-//   const hostname = domain.replace("http://", "")
-//   const secondsSinceIssued = getEpochSinceIssued()
-//   const bodyHash = createBase64Hash(JSON.stringify(payload))
-//   const nonce = `${secondsSinceIssued}:${randomString}`
-
-//   const macString = `${nonce}\n${method}\n${route}\n${hostname}\n${targetPort}\n${bodyHash}\n\n`
-//   const mac = createHMACBase64Hash(macString)
-//   return `MAC id='${serverId}',nonce='${nonce}',bodyhash='${bodyHash}',mac='${mac}'`
-// }
-
-// const macAuth = createHMACAuth(baseURL, "/", "POST", newMessage)
-// console.log({
-//   headers: {
-//     Authorization: macAuth
-//   }
-// })
-
-//
-//  Do the request
-//
-
-// const https = require('https')
 const http = require("http")
 
-const data = JSON.stringify(newMessage)
+// Setup data and various, necessary transformations
+const secret = "secret"
+const secretBuffer = Buffer.from(secret, "base64")
 
-const options = {
+const message = {
+  text: "helloworld from team awesome (dan, and lee)"
+}
+const payload = JSON.stringify(message)
+const payloadBuffer = Buffer.from(payload, "utf8")
+
+// Create HMAC hash
+const hash = crypto.createHmac("sha256", secretBuffer).update(payloadBuffer)
+const digest64 = hash.digest("base64")
+const hmacAuth = `HMAC ${digest64}`
+
+const httpRequestOptions = {
   hostname: "localhost",
-  port: 8011,
-  path: "/api/messages",
+  port: 8080,
+  path: "/",
   method: "POST",
   headers: {
     "Content-Type": "application/json",
-    // authentication: macAuth,
-    "Content-Length": data.length
+    Authorization: hmacAuth,
+    "Content-Length": payload.length
   }
 }
 
-const req = http.request(options, res => {
-  console.log(`statusCode: ${res.statusCode}`)
-
+// Send the request
+const req = http.request(httpRequestOptions, res => {
   res.on("data", d => {
     process.stdout.write(d)
   })
 })
-
 req.on("error", error => {
   console.error(error)
 })
-
-req.write(data)
+req.write(payload)
 req.end()
