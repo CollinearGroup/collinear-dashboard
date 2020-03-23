b5edf9643c31631186ad83c85d7cc4e273e2046f
Garret Bolton <Garret.E.Bolton@boeing.com>
2020-03-23 09:01:36 -0700

    formatted message cards to match mockup

--- dashboard/src/message-board/components/Message.jsx
+++ dashboard/src/message-board/components/Message.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import './Message.css'
-import moment from 'moment'
+import React from "react";
+import "./Message.css";
+import moment from "moment";
 
-const Message = (props) => {
-    return (
-        <div className='message-container'>
-            <div className='poster'>{props.poster_name}</div>
-            <div className='message-date'>  {moment(props.show_from).format('MMMM Do, YYYY')}</div>
-            <div className='message'>{props.message}</div>
-        </div>
-    )
-}
+const Message = props => {
+  return (
+    <div className="message-container">
+      <div className="poster">{props.poster_name}</div>
+      <div className="message-date">
+        {moment(props.show_from).format("MMMM Do, YYYY")}
+      </div>
+      <br></br>
+      <p className="message">{props.message}</p>
+    </div>
+  );
+};
 
-export default Message
+export default Message;
