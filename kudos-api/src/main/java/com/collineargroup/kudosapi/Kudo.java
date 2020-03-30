1415de1ab9df9ace3c59151a544be58f0312dbdb
Dan Villa <dvilla@collineargroup.com>
2020-03-30 07:53:36 -0700

    feat: connect to mysql

--- kudos-api/src/main/java/com/collineargroup/kudosapi/Kudo.java
+++ kudos-api/src/main/java/com/collineargroup/kudosapi/Kudo.java
@@ -1,9 +1,13 @@
 package com.collineargroup.kudosapi;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
 
 import lombok.Data;
 
@@ -17,6 +21,8 @@ public class Kudo {
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
   private String message;
-  private String from_person;
-  private String to_person;
+  private String fromPerson;
+  private String toPerson;
+  @CreationTimestamp
+  private Date createdDate;
 }
