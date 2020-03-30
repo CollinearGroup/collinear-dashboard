7edfcb9abdfe0131509412aa5f7d10c71af15137
Dan Villa <dvilla@collineargroup.com>
2020-03-30 00:04:06 -0700

    feat: use simple crud repo and ensure values are get and set correctly

--- kudos-api/src/main/java/com/collineargroup/kudosapi/Kudo.java
+++ kudos-api/src/main/java/com/collineargroup/kudosapi/Kudo.java
@@ -1,27 +1,22 @@
 package com.collineargroup.kudosapi;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Data;
+
 /**
  * Kudos
  */
 @Entity
+@Data
 public class Kudo {
-  protected Kudo() {}
-  public Kudo(String message) {
-    this.message = message;
-  }
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
   private String message;
-  
-  /**
-   * @return the message
-   */
-  public String getMessage() {
-    return message;
-  }
+  private String from_person;
+  private String to_person;
 }
--- kudos-api/src/main/java/com/collineargroup/kudosapi/KudoRepository.java
+++ kudos-api/src/main/java/com/collineargroup/kudosapi/KudoRepository.java
@@ -1,10 +1,11 @@
 package com.collineargroup.kudosapi;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  * KudosRepository
  */
-public interface KudoRepository extends CrudRepository<Kudo, Long>{
-
-  
+@RepositoryRestResource(collectionResourceRel = "kudo", path = "kudo")
+public interface KudoRepository extends CrudRepository<Kudo, Long> {
 }
