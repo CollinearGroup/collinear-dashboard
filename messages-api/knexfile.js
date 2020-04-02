4575636efbc4456a6216290c20c1ba22289dde13
Dan Villa <dvilla@collineargroup.com>
2020-04-02 09:13:56 -0700

    feat: add db compose

--- messages-api/knexfile.js
+++ messages-api/knexfile.js
@@ -20,12 +20,7 @@ module.exports = {
   },
   production: {
     client: "pg",
-    connection: {
-      host: "messages_database",
-      user: POSTGRES_USER,
-      password: POSTGRES_PASSWORD,
-      database: POSTGRES_DB
-    },
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: path.join(__dirname, "database", "migrations")
     }
