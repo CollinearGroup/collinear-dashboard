04c3c3031b0f6d85a1775db7313633f800c81a97
Dan Villa <dvilla@collineargroup.com>
2020-04-03 07:41:14 -0700

    feat: use registry images and make them small

--- README.md
+++ README.md
@@ -29,6 +29,13 @@ $ ./setUpDb.sh
 $ docker-compose up -d
 ```
 
+### Deploy
+
+Currently we publish 2 containers due to some limitations of the production environment.
+Build and publish those with the `./publish.sh` script.
+
+Then refer to the [collinear-dashboard-server-config](https://gitlab.com/collinear/collinear-dashboard-server-config) repo for deployment configs.
+
 ## Contributing
 
 See our [Confluence](https://collineargroup.atlassian.net/wiki/spaces/COL/pages/687538280/Dev+Ops)
---
+++ publish.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Dashboard
+cd dashboard;
+docker build . -t dashboard;
+docker tag dashboard registry.gitlab.com/collinear/collinear-dashboard-server-config/dashboard;
+docker push registry.gitlab.com/collinear/collinear-dashboard-server-config/dashboard;
+cd -;
+
+
+# Pictures API
+cd picture-uploader;
+docker build . -t picture-uploader;
+docker tag picture-uploader registry.gitlab.com/collinear/collinear-dashboard-server-config/picture-uploader;
+docker push registry.gitlab.com/collinear/collinear-dashboard-server-config/picture-uploader;
+cd -;
+
+# Calendar API
+cd calendar-api;
+docker build . -t calendar-api;
+docker tag calendar-api registry.gitlab.com/collinear/collinear-dashboard-server-config/calendar-api;
+docker push registry.gitlab.com/collinear/collinear-dashboard-server-config/calendar-api;
+cd -;
+
+# Foosball API
+cd foosball-api;
+docker build . -t foosball-api;
+docker tag foosball-api registry.gitlab.com/collinear/collinear-dashboard-server-config/foosball-api;
+docker push registry.gitlab.com/collinear/collinear-dashboard-server-config/foosball-api;
+cd -;
+
+# Message Board API
+cd messages-api;
+docker build . -t messages-api;
+docker tag messages-api registry.gitlab.com/collinear/collinear-dashboard-server-config/messages-api;
+docker push registry.gitlab.com/collinear/collinear-dashboard-server-config/messages-api;
+cd -;
+
+# Kudos
+cd kudos-api;
+./gradlew jibDockerBuild --image=kudos-api;
+docker tag kudos-api registry.gitlab.com/collinear/collinear-dashboard-server-config/kudos-api;
+docker push registry.gitlab.com/collinear/collinear-dashboard-server-config/kudos-api;
+cd -;
